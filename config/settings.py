3ab14d69f5440880a78a78618947a93c3fd18449
bethneyanderson <bethneyanderson95@gmail.com>
2025-07-28 14:55:20 +0100

    Add register page

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Authentication settings
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import home_page_view
+from .views import home_page_view, register_view
 
 
 
 urlpatterns = [
     path('', home_page_view, name='home'),
+    path('register/', register_view, name='register'),
     path('summernote/', include('django_summernote.urls')),
 
 ]
