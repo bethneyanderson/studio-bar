7417a1c2e7a1a7d77c77d300207271151902614b
bethneyanderson <bethneyanderson95@gmail.com>
2025-08-07 10:19:14 +0100

    Change DEBUG setting to deploy False

--- config/settings.py
+++ config/settings.py
@@ -16,9 +16,8 @@ import dj_database_url
 
 if os.path.isfile('env.py'):
     import env  # noqa: F401
-    DEBUG = True
-else:
-    DEBUG = False
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- env.py
+++ env.py
@@ -7,3 +7,5 @@ os.environ.setdefault(
 os.environ.setdefault(
     'DATABASE_URL',
     'postgresql://neondb_owner:npg_9zjNdUFYvi2Z@ep-lingering-night-a2q2afdh.eu-central-1.aws.neon.tech/dart_dry_smirk_228448')
+
+os.environ.setdefault('DEBUG', 'True')
